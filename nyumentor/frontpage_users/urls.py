734ada2531968f87ebb48aed07222f6fd9c98ace
Tom Wall <twall@millennialmedia.com>
2015-04-27 19:45:37 -0400

    changing frontpage users to have inbox link option

--- nyumentor/frontpage_users/urls.py
+++ nyumentor/frontpage_users/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import patterns, url 
-from frontpage_users import views 
+from django.conf.urls import patterns, url
+from frontpage_users import views
 
 urlpatterns = patterns('',
 	url(r'^user_profile/$', views.user_profile, name='profile'),
